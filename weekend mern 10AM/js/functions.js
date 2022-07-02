7b6bcd0296087559a61b75eeb1246465a8d36b9b
deepaksinghducat <deepaksinghducat@gmail.com>
2022-07-02 12:46:57 +0530

    02-july-2022

---
+++ weekend mern 10AM/js/functions.js
@@ -0,0 +1,213 @@
+// // Functions
+// function xyz() {
+
+// }
+
+// //how to use 
+// xyz();
+
+// // parameters and arguments
+// function abc(parameter1, parm2,param3) {
+//     console.log(parameter1,parm2,param3);
+// }
+
+
+// abc(10,20,30);
+// function abc(parameter1, parm2,param3 = 10) {
+//     var a = parameter1;
+//     var b = param3;
+    
+//     console.log(a,b);
+// }
+
+// abc(10,20,55454545);
+
+// var array = [10,20,30,40];
+
+// array.forEach(function(element){
+//  console.log(element);
+// })
+
+// function abc(parameter1, parm2,param3) {
+
+//     abc(10,20,30);
+
+// }
+
+
+// arrow function
+// let a = a => "fafasfasf";
+
+// let a = a => console.log(a);
+
+// a(10);
+
+// rest operators
+
+var data = [
+
+]
+
+function a(...rest) {
+    console.log(rest);
+}
+
+// a(10,20,30,40,50,60);
+
+// var object = {
+//     name: "fasfa",
+//     age: 20
+// }
+
+
+// function a() {
+//     function b() {
+//         console.log("function b");
+//     }
+//     b();
+//     console.log("function a");
+// }
+// a();
+
+
+// 153 = 1*1*1 +5*5*5+3*3*3
+
+
+// function armstrongNumber(number) {
+    
+//     var num = 0;
+
+//     var num1 = number;
+    
+//     while(num1 > 0) {
+//         var remainder = parseInt(num1 % 10);
+
+//         num = num + remainder * remainder * remainder ; 
+
+//         num1 =parseInt( num1 /10 );
+//     }
+
+//     if(num === number) {
+//         console.log("armstrong number")
+//     }else{
+//         console.log("not a armstrong number");
+//     }
+// }
+
+// armstrongNumber(153);
+
+
+
+
+
+
+
+
+
+//function
+
+// function fasfasdf() {
+
+// }
+
+// 
+
+// fasfasdf();
+
+
+// parameter and arguments
+
+// function abc(p1,p2,p3) {
+//     console.log(p1,p2,p3);
+// }
+
+
+// abc(10,20,30);
+
+//function
+// function abc()
+//  {
+//     console.log('fasfsa');
+//  }
+
+
+
+// class name {
+//     name() {
+        
+//     }
+// }
+
+// var object = new name()
+// name.name()
+
+var a = 0;
+
+
+// default argument
+// function abc(p1,p2 = 10) {
+//     a = p1;
+//     console.log(p2);
+// }
+
+// abc(30);
+
+
+// Anonymous functions
+// var array = [10,20,30,40,50,60];
+
+// var data = array.forEach(function(element) {
+//     element += 10;
+
+//     return element;
+// });
+
+// console.log(data);
+
+
+
+// function abc() {
+//     function xyz() {
+//         console.log("xyz");
+//     }
+
+
+//     console.log("abc");
+// }
+
+// abc().xyz();
+
+
+// rest operators
+
+
+// function abc(param1,param2, ...rest) {
+//     console.log(rest);
+// }
+
+
+// ?abc(10,20,30,40,50);
+
+
+// const person = {
+//     firstName : "fsafsdaf",
+//     lastName : "fasfsafd",
+//     fullName: function () {
+//         return this.firstName+ " "+this.lastName 
+//     }
+// }
+
+
+// const member = {
+//     firstName: "fafsafsdaf",
+//     lastName: "fasfsafdfasfasfd"
+// }
+
+// let fullName = person.fullName.bind(member);
+
+// console.log(fullName());
+
+
+
+
+
