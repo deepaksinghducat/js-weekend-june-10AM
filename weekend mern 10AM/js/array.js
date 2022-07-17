b767fbe909a342bce29f2a0207f349a09dfe28b6
deepaksinghducat <deepaksinghducat@gmail.com>
2022-07-17 11:49:50 +0530

    upto array

---
+++ weekend mern 10AM/js/array.js
@@ -0,0 +1,141 @@
+// Array 
+
+// 1. []
+// 2. new Array()
+// 3. Array.of()
+// 4. Array.from()
+
+// []
+
+// const array = [10,20,30,40];
+
+// console.log(array);
+
+// new Array()
+
+// const array = new Array(5,20);
+// console.log(array);
+
+// Array Of
+
+// const array = Array.of(10,20,30,40);
+// console.log(array);
+
+// Array from
+
+// const array = Array.from([10])
+
+// console.log(array);
+
+
+// page speed reduce
+
+// 1. 10 instance 4 byte
+// 2. 1 instance 4 byte
+
+
+// PUSH , POP , SHIFT , UNSHIFT
+
+// let array = [10,20,30,40,50];
+
+// array.push({key:"fafdas"});
+
+// console.log(array);
+
+// let lastIndex = array.pop();
+
+// console.log(array,lastIndex);
+
+// array.shift();
+
+// array.unshift(10);
+
+// console.log(array);
+
+// IndexOf
+
+// let array = [10,20,30,40,50];
+
+// const index = array.indexOf(30);
+
+// delete array[4];
+
+// array.splice(index,3);
+
+// console.log(array);
+
+// slice 
+// let array = [10,20,30,40,50];
+
+// let subArray = array.slice(0,2);
+
+// console.log(array,subArray);
+
+// Find
+
+// let array = [10,20,30,40,50,40];
+
+// console.log(array.find((number) => {
+//     if(number === 40) return number;
+// }));
+
+// Filter
+// let array = [10,20,30,40,50,40];
+
+// console.log(array.indexOf(40));
+
+// console.log(array.filter((number) => {
+//     if(number === 40) return number;
+// }));
+
+// Map 
+
+// let array = [10,20,30,40,50,40];
+
+// let multiplyBy2 = array.map((element) => {
+//     return element * 2;
+// });
+
+// console.log(multiplyBy2);
+
+
+// Includes 
+
+// let array = [10,20,30,40,50,40];
+
+// console.log(array.includes(40));
+
+// Sort | reverse
+// let array = [10,20,30,40,50,40];
+
+// console.log(array.sort());
+
+// console.log(array.reverse());
+
+// concat()
+
+// let array = [10,20,30,40,50,40];
+
+// let array2 = [10,20,30,40,50,40];
+
+// let array3 = array.concat(array2);
+
+// console.log(array3);
+
+
+// let array = [10,20,30,40,50,40];
+
+// array.push(40)
+
+
+
+
+
+
+
+
+
+
+
+
+
